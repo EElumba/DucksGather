102c62108259235018574ce630b3ee9b2cf08ea2
raull <rlopezt11@gmail.com>
2025-11-25 13:09:43 -0800

    docker functionality

--- backend/src/app.py
+++ backend/src/app.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
 	#Set debug mode to False in production
 	#Host0.0.0.0 to be accesible in LAN
 	debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "yes")
-	app.run(host="127.0.0.1", port=5000, debug=debug_mode)
+	app.run(host="0.0.0.0", port=5000)
